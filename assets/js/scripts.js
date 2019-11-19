a8b9feb3c0ccabe2205064b222487ee4b5b47937
Helberth Caro <31378229+helberthCO@users.noreply.github.com>
2019-11-18 20:30:27 -0500

    initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+(function () {
+    const addClassOnClick = (classToAdd, trigger, target = false) => {
+        let triggerElem = document.querySelector(trigger),
+            targetElem = document.querySelector(target !== false ? target : '');
+        if(triggerElem != null){
+            triggerElem.addEventListener('click', _=>{
+                triggerElem.classList.toggle(classToAdd);
+                if(targetElem !== null){
+                    targetElem.classList.toggle(classToAdd);
+                }
+            })
+        }
+    };
+    addClassOnClick('menu-open', '.js-menu-trigger', '.js-menu-target');
+})();
